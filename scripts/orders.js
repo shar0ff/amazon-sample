1223bda04da2860cf344b6a5cdf66611a0051779
shar0ff <sharovivan17@gmail.com>
2025-05-21 00:01:19 +0200

    Make buy again button interactive

--- scripts/orders.js
+++ scripts/orders.js
@@ -2,6 +2,7 @@ import {getProductById, loadProducts} from '../data/products.js';
 import {orders} from '../data/orders.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { formatCurrency } from './helpers/currency.js';
+import {cart} from '../data/cart.js';
 
 async function loadPage() {
     await loadProducts();
@@ -36,47 +37,63 @@ async function loadPage() {
         `;
     });
 
+    function productsListHTML(order) {
+        let productsListHTML = '';
+
+        order.products.forEach((productDetails) => {
+            const product = getProductById(productDetails.productId);
+
+            productsListHTML += `
+                <div class="product-image-container">
+                <img src="${product.image}">
+                </div>
+                <div class="product-details">
+                <div class="product-name">
+                    ${product.name}
+                </div>
+                <div class="product-delivery-date">
+                    Arriving on: ${
+                    dayjs(productDetails.estimatedDeliveryTime).format('MMMM D')
+                    }
+                </div>
+                <div class="product-quantity">
+                    Quantity: ${productDetails.quantity}
+                </div>
+                <button class="buy-again-button button-primary js-buy-again" data-product-id=${productDetails.productId} data-product-quantity=${productDetails.quantity}>
+                    <img class="buy-again-icon" src="images/icons/buy-again.png">
+                    <span class="buy-again-message">Buy it again</span>
+                </button>
+                </div>
+                <div class="product-actions">
+                <a href="tracking.html?orderId=${order.id}&productId=${product.id}">
+                    <button class="track-package-button button-secondary">
+                    Track package
+                    </button>
+                </a>
+                </div>
+            `;
+        });
+
+        return productsListHTML;
+    }
+
     document.querySelector('.js-orders-grid').innerHTML = ordersHTML;
-}
 
-function productsListHTML(order) {
-    let productsListHTML = '';
+    document.querySelectorAll('.js-buy-again').forEach((button) => {
+        button.addEventListener('click', () => {
+        cart.addToCart(button.dataset.productId, Number(button.dataset.productQuantity));
+        cart.updateCartQuantity();
 
-    order.products.forEach((productDetails) => {
-      const product = getProductById(productDetails.productId);
-
-      productsListHTML += `
-        <div class="product-image-container">
-          <img src="${product.image}">
-        </div>
-        <div class="product-details">
-          <div class="product-name">
-            ${product.name}
-          </div>
-          <div class="product-delivery-date">
-            Arriving on: ${
-              dayjs(productDetails.estimatedDeliveryTime).format('MMMM D')
-            }
-          </div>
-          <div class="product-quantity">
-            Quantity: ${productDetails.quantity}
-          </div>
-          <button class="buy-again-button button-primary">
+        button.innerHTML = 'Added';
+        setTimeout(() => {
+            button.innerHTML = `
             <img class="buy-again-icon" src="images/icons/buy-again.png">
             <span class="buy-again-message">Buy it again</span>
-          </button>
-        </div>
-        <div class="product-actions">
-          <a href="tracking.html?orderId=${order.id}&productId=${product.id}">
-            <button class="track-package-button button-secondary">
-              Track package
-            </button>
-          </a>
-        </div>
-      `;
+            `;
+        }, 1000);
+        });
     });
-
-    return productsListHTML;
 }
 
 loadPage();
+cart.updateCartQuantity();
